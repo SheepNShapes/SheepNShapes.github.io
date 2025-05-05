d8c325f1ae0f76c8435ce3ec132524f644ddc44a
Klara-the-sheep <209606401+SheepNShapes@users.noreply.github.com>
2025-05-05 10:43:24 +0200

    Neue Version Mobile Variante

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeImageDesktop(x,image)
+{
+    if(x==1) {
+        image.src = 'Assets/Buttons/Desktop_Button_hover_big.PNG';
+    }
+    if(x==2) {
+        image.src = 'Assets/Buttons/Desktop_Button_big.png';
+    }
+}
+
+function changeImageMobile(x,image)
+{
+    if(x==1) {
+        image.src = 'Assets/Buttons/Mobile_Button_Hover_big.PNG';
+    }
+    if(x==2) {
+        image.src = 'Assets/Buttons/Mobile_Button_big.png';
+    }
+}
